80ca6654f62af73fd09828a460d1bd584efeaff4
Adrian DC <radian.dc@gmail.com>
2018-02-09 19:39:02 +0000

    CameraWrapper: update library dependencies for O

--- camera-wrapper/Android.mk
+++ camera-wrapper/Android.mk
@@ -7,7 +7,21 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
+<<<<<<< HEAD:camera/Android.mk
     libhardware liblog libcamera_client libgui libutils libbase
+=======
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+>>>>>>> f1b267e... CameraWrapper: update library dependencies for O:camera-wrapper/Android.mk
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
