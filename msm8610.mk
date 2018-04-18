d23979587d658f99c78469b5f67f7d0c31d5f794
Adrian DC <radian.dc@gmail.com>
2018-04-18 12:13:02 +0100

    Hal1 changes

--- msm8610.mk
+++ msm8610.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8610 \
     Snap
