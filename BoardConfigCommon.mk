b2a65363648af72e5ecaa121acab8aac9ce445ec
Kyle Harrison <khwebmail@gmail.com>
2018-04-23 21:29:57 +0100

    msm8916-common: Build custom libril

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,9 @@ TARGET_RECOVERY_DENSITY := hdpi
 BOARD_HAS_NO_SELECT_BUTTON := true
 HAVE_SELINUX := true
 
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
