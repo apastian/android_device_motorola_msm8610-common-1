872a7f77390cf81c268dd6579753ca1806420d2a
Sushant Kumar <rahul.kr.sushant@gmail.com>
2018-04-23 15:44:55 +0000

    more cam changes

--- camera-wrapper/Android.mk
+++ camera-wrapper/Android.mk
@@ -7,9 +7,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-<<<<<<< HEAD:camera/Android.mk
     libhardware liblog libcamera_client libgui libutils libbase
-=======
     libhardware \
     liblog \
     libcamera_client \
@@ -21,7 +19,6 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_STATIC_LIBRARIES := \
     libarect
->>>>>>> f1b267e... CameraWrapper: update library dependencies for O:camera-wrapper/Android.mk
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
