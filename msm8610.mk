779ae122aa8c0ae98f29d597f07363b43c849da2
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-05-01 19:23:13 +0530

    Changes for legacy camera

--- msm8610.mk
+++ msm8610.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@3.2-impl \
+    camera.device@1.0-impl-legacy \
     camera.msm8610 \
     Snap
 
