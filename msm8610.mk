0ba555f6a8202e5bdcc483c9944ada1b053001ca
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-11-20 14:47:31 +0000

    msm8610-common: Build configstore service

--- msm8610.mk
+++ msm8610.mk
@@ -118,6 +118,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.configstore@1.1-service \
     gralloc.msm8610 \
     copybit.msm8610 \
     hwcomposer.msm8610 \
