b2bd0b4b051c1b30ff9c62c01760e50b7a0ae704
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-11-20 06:16:13 +0000

    Change the sepolicy to legacy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,7 +137,7 @@ HAVE_SELINUX := true
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # HIDL
